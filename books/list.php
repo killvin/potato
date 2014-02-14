77093a819fd65ea400bb774b9db7bf595805b93f
killvin <killvin@hotmail.com>
2014-02-14 17:40:33 +0800

    update css file

---
+++ books/list.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="zh-CN" class="ua-windows ua-ff26 book-new-nav">
+<head>
+	<meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+	<title> Search List </title>
+	<link type="text/css" rel="stylesheet" href="../css/master.css">
+	<link type="text/css" rel="stylesheet" href="../css/global.css">
+</head>
+
+<body>
+
+<div class="global-nav" id="db-global-nav">
+	<div class="bd">
+		<div class="top-nav-info">
+			<a rel="nofollow" class="nav-login" href="">SIGN ON</a>
+			<a rel="nofollow" class="nav-register" href="">SIGN IN</a>
+		</div>
+
+		<div class="global-nav-items">
+			<ul>
+				<li>
+					<a data-moreurl-dict="{&quot;from&quot;:&quot;top-nav-click-main&quot;,&quot;uid&quot;:&quot;0&quot;}" target="_blank" href="">##</a>
+				</li>
+				<li class="on">
+					<a data-moreurl-dict="{&quot;from&quot;:&quot;top-nav-click-book&quot;,&quot;uid&quot;:&quot;0&quot;}" href="">##</a>
+				</li>
+				<li>
+					<a class="bn-more" href="#more"><span>MORE</span></a>
+					<div class="more-items">
+						<table cellspacing="0" cellpadding="0">
+							<tbody>
+								<tr>
+									<td><a data-moreurl-dict="{&quot;from&quot;:&quot;top-nav-click-9&quot;,&quot;uid&quot;:&quot;0&quot;}" target="_blank" href="">##</a></td>
+								</tr>
+								<tr>
+									<td><a data-moreurl-dict="{&quot;from&quot;:&quot;top-nav-click-town&quot;,&quot;uid&quot;:&quot;0&quot;}" target="_blank" href="">##</a></td>
+								</tr>
+								<tr>
+									<td><a data-moreurl-dict="{&quot;from&quot;:&quot;top-nav-click-mobile&quot;,&quot;uid&quot;:&quot;0&quot;}" target="_blank" href="">##</a></td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</div>
+</div>
+
+<div class="nav" id="db-nav-book">
+	<div class="nav-wrap">
+		<div class="nav-primary">
+			<div class="nav-logo">
+				<a href="">##</a>
+			</div>
+			<div class="nav-search">
+				<form method="get" action="">
+					<fieldset>
+						<legend>SEARCH��</legend>
+						<label for="inp-query" style="display: none;">BOOK、AUTHOR、ISBN</label>
+						<div class="inp">
+							<input value="ruby" maxlength="60" size="22" name="search_text" id="inp-query" placeholder="BOOK、AUTHOR、ISBN" autocomplete="off" dir="ltr">
+						</div>
+						<div class="inp-btn">
+							<input type="submit" value="SEARCH">
+						</div>
+						<input type="hidden" value="1001" name="cat">
+					</fieldset>
+				</form>
+			</div>
+		</div>
+	</div>
+	<div class="nav-secondary">
+		<ul class="nav-items">
+			<li class="nav-item-first"><a href="">####</a></li>
+			<li class="book-cart"><a href="">###</a></li>
+			<li>
+				<a target="_blank" href="">####</a>
+				<sup class="icon-new"></sup>
+			</li>
+		</ul>
+	</div>
+</div>
+
+
+<div id="wrapper">
+	<div id="content">
+		<div class="grid-16-8 clearfix">
+			<div class="article">
+				<div class="trr">SEARCH RESULT 1-1 &nbsp; 1</div>
+				<ul class="subject-list">
+					<li class="subject-item">
+						<div class="pic">
+							<a }="" onclick="&quot;moreurl(this,{i:'0'})&quot;" href="" class="nbg">
+								<img width="90" src="../images/s26393136.jpg" class="">
+							</a>
+						</div>
+					
+						<div class="info">
+							<h2 class="">
+							<a onclick="&quot;moreurl(this,{i:'0'})&quot;" title="{BOOK NAME}" href="">{BOOK NAME}
+								<span style="font-size:12px;"> :{BOOK STYPE} </span>
+							</a>
+							</h2>
+							<div class="pub"> {AUTHOR / TRANSLATOR / PUBLISHER / DATE / PRICE}</div>
+							<div class="star clearfix">
+								<span class="allstar40"></span>
+								<span class="rating_nums">{RATING}</span>
+								<span class="pl">{REMARKS}</span>
+							</div>
+						
+							<div class="ft">
+								<div class="collect-info"></div>
+								<div class="buy-info">
+									<a href="" target="_blank">{EE PRICE}</a>
+									<span class="splitter">|</span>
+									<a href="">{PE PRICE}</a>
+								</div>
+							</div>
+						</div>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
+
+</body>
+</html>
